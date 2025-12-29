83156d62d3904621adac1c1a385295de4385ef6d
筱夕Sushi <kirastar_shannei@163.com>
2025-12-29 23:09:42 +0800

    移除盒子，添加水晶

---
+++ Assets/#Template/[Scripts]/Trigger/Crystal.cs
@@ -0,0 +1,65 @@
+using DancingLineFanmade.Level;
+using UnityEngine;
+
+namespace DancingLineFanmade.Trigger
+{
+    [DisallowMultipleComponent, RequireComponent(typeof(Collider), typeof(MeshRenderer))]
+    public class Crystal : MonoBehaviour
+    {
+        private Player player;
+        private GameObject effectPrefab;
+        private GameObject effect;
+        private bool got = false;
+        private int index;
+
+        private MeshRenderer MeshRenderer
+        {
+            get => GetComponent<MeshRenderer>();
+        }
+
+        private void Start()
+        {
+            MeshRenderer.enabled = true;
+            effectPrefab = Resources.Load<GameObject>("Prefabs/FX_GetCrystal");
+            player = Player.Instance;
+        }
+
+        private void Update()
+        {
+            transform.Rotate(Vector3.up, Time.deltaTime * 40f);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player") && !got)
+            {
+                got = true;
+                player.Events?.Invoke(6);
+                MeshRenderer.enabled = false;
+                index = player.Checkpoints.Count;
+                if (QualitySettings.GetQualityLevel() > 0)
+                {
+                    effect = Instantiate(effectPrefab, transform.position, Quaternion.Euler(-90, 0, 0));
+                }
+
+                LevelManager.revivePlayer += ResetData;
+            }
+        }
+
+        private void ResetData()
+        {
+            LevelManager.revivePlayer -= ResetData;
+            LevelManager.CompareCheckpointIndex(index, () =>
+            {
+                got = false;
+                MeshRenderer.enabled = true;
+            });
+            if (effect) Destroy(effect);
+        }
+
+        private void OnDestroy()
+        {
+            LevelManager.revivePlayer -= ResetData;
+        }
+    }
+}
